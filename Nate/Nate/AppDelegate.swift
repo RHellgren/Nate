f4cf3737b2f8ab5c3da9eca1592861b009ad8258
RHellgren <rhellgren@icloud.com>
2020-08-18 22:36:19 +0100

    Create empty project

---
+++ Nate/Nate/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Nate
+//
+//  Created by Robin Hellgren on 18/08/2020.
+//  Copyright © 2020 Robin Hellgren. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+}
---
+++ Nate/Nate/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Nate
+//
+//  Created by Robin Hellgren on 18/08/2020.
+//  Copyright © 2020 Robin Hellgren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
